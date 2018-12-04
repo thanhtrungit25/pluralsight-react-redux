b885c579abe6211383f36f1978b6e0ce6e77fb87
TrungDT <thanhtrung.it25@gmail.com>
2018-12-04 11:15:26 +0700

    Handle creates and updates in reducer courses

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,10 +1,19 @@
 import * as types from "./actionTypes";
 import courseApi from "../api/mockApiCourseApi";
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function createCourseSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course
+  };
 }
 
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course
+  };
+}
 export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
@@ -16,6 +25,16 @@ export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      savedCourse.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
