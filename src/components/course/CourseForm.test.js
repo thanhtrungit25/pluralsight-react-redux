a601c91f91329152c725018e838e251154f65b73
TrungDT <thanhtrung.it25@gmail.com>
2018-12-04 14:21:35 +0700

    Throw in some tests using React Test Utils

---
+++ src/components/course/CourseForm.test.js
@@ -0,0 +1,45 @@
+import expect from "expect";
+import React from "react";
+import TestUtils from "react-addons-test-utils";
+import CourseForm from "./CourseForm";
+
+function setup(saving) {
+  let props = {
+    course: {},
+    saving,
+    errors: {},
+    onSave: () => {},
+    onChange: () => {}
+  };
+
+  let renderer = TestUtils.createRenderer();
+  renderer.render(<CourseForm {...props} />); 
+  let output = renderer.getRenderOutput();
+
+  return {
+    renderer,
+    output,
+    props
+  };
+}
+
+describe("CourseForm via React Test Utils", () => {
+  it("render form and h1", () => {
+    const { output } = setup();
+    expect(output.type).toBe("form");
+    let [ h1 ] = output.props.children;
+    expect(h1.type).toBe("h1");
+  });
+  it("Save button is lablled 'Save' when not saving", () => {
+    const { output } = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe("Save");
+  });
+  it("Save button is lablled 'Saving' when save", () => {
+    const { output } = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe("Saving...");
+  });
+});
+
+
