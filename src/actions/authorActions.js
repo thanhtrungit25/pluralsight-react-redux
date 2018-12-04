9d26a65fa1be4c82735f9c31553fb35d56fd132c
TrungDT <thanhtrung.it25@gmail.com>
2018-12-04 11:06:07 +0700

    Create Save Course Thunk

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,20 @@
 import * as types from "./actionTypes";
 import authorApi from "../api/mockAuthorApi";
+import courseApi from "../api/mockApiCourseApi";
+
+export function createCourseSuccess(courses) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    courses
+  };
+}
+
+export function updateCourseSuccess(courses) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    courses
+  };
+}
 
 export function loadAuthorsSuccess(authors) {
   return {
@@ -12,6 +27,16 @@ export function loadAuthors() {
   return function(dispatch) {
     return authorApi.getAllAuthors(authors => {
       dispatch(loadAuthorsSuccess(authors));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      savedCourse.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
