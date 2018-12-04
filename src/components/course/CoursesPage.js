559e05d0a51056eb85e1d85e961cc386e3e9e2f7
TrungDT <thanhtrung.it25@gmail.com>
2018-12-04 09:43:13 +0700

    Clean up Courses page container

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,29 +2,11 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { Link } from "react-router";
 import * as courseActions from "../../actions/courseActions";
  
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-    this.setState({ course: { title: "" } });
   }
 
   courseRow(course, index) {
@@ -36,14 +18,6 @@ class CoursesPage extends Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          value={this.state.course.title}
-          onChange={this.onTitleChange} />
-        <input
-          type="submit"
-          onClick={this.onClickSave}value="Save" />
       </div>
     );
   }
@@ -62,7 +36,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
