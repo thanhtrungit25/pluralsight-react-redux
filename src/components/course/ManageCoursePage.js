88301c905c21b77d7e4d17d20c64ad8de0a2e733
TrungDT <thanhtrung.it25@gmail.com>
2018-12-04 15:15:08 +0700

    Test mapStateToProps using selectors

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import * as courseActions from "../../actions/courseActions";
 import CourseForm from "./CourseForm";
 import toastr from "toastr";
+import { authorsFormattedForDropdown } from "../../selectors/selectors";
 
 export class ManageCoursePage extends Component {
   constructor(props, context) {
@@ -104,16 +105,9 @@ const mapStateToProps = (state, ownProps) => {
     course = getCourseById(state.courses, courseId);
   } 
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: `${author.firstName} ${author.lastName}`
-    };
-  });
-
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 };
 
